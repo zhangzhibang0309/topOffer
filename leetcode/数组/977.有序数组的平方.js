19f1bd75780dd2b2c58841b2d78bfa2e9187f382
zhangzhibang0309 <295854713@qq.com>
2022-02-01 22:54:02 +0800

    有序数组的二平方

---
+++ leetcode/数组/977.有序数组的平方.js
@@ -0,0 +1,33 @@
+/**
+ * time 2022.02.01
+ *
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+// 暴力解法 先平方 再排序
+// 时间复杂度 如果sort是快排的话O(n + n\log n) 空间复杂度O(n)
+var sortedSquares = function (nums) {
+  const newArray = nums.map((item) => {
+    return item ** 2;
+  });
+  return newArray.sort((a, b) => a - b);
+};
+
+// 时间复杂度O(n) 空间复杂度O(n)
+var sortedSquares = function (nums) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  const newArray = [];
+  while (left <= right) {
+    let max = 0;
+    if (nums[left] ** 2 > nums[right] ** 2) {
+      max = nums[left++];
+    } else {
+      max = nums[right--];
+    }
+    newArray.unshift(max ** 2);
+  }
+
+  return newArray;
+};

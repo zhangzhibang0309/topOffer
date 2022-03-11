e09d9d4368ef47e08f45c58408b5edbba5a8bfa9
zhangzhibang0309 <295854713@qq.com>
2022-03-11 19:53:06 +0800

    update

---
+++ JS手写题/20_防抖和节流,.js
@@ -0,0 +1,48 @@
+// mark
+
+// 防抖
+function debounce(fn, delay = 300) {
+  //默认300毫秒
+  let timer;
+  console.log(this)
+  return function () {
+    const args = arguments;
+    if (timer) {
+      clearTimeout(timer);
+    }
+    timer = setTimeout(() => {
+      fn.call(this,args); // 改变this指向为调用debounce所指的对象
+    }, delay);
+  };
+}
+
+let obj = {
+    debounce
+}
+window.addEventListener(
+  "scroll",
+  obj.debounce(function() { 
+    console.log(111,this);
+  }, 1000)
+);
+
+// // 节流
+// // 设置一个标志
+// function throttle(fn, delay) {
+//   let flag = true;
+//   return () => {
+//     if (!flag) return;
+//     flag = false;
+//     timer = setTimeout(() => {
+//       fn();
+//       flag = true;
+//     }, delay);
+//   };
+// }
+
+// window.addEventListener(
+//   "scroll",
+//   throttle(() => {
+//     console.log(111);
+//   }, 1000)
+// );

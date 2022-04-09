2cd933abdebda22a5037c22eb437d49304c9b460
zhangzhibang0309 <295854713@qq.com>
2022-04-09 23:55:57 +0800

    数组去重补充

--- JS手写题/04_数组去重.js
+++ JS手写题/04_数组去重.js
@@ -1,20 +1,38 @@
 // es6
-function uniqueArr(arr) {
-  return [...new Set(arr)];
-}
-const newArr = uniqueArr([1,1,2,2,3,45,6,4,2,11])
-console.log(newArr)
+// function uniqueArr(arr) {
+//   return [...new Set(arr)];
+// }
+// const newArr = uniqueArr([1,1,2,2,3,45,6,4,2,11])
+// console.log(newArr)
 
-// 不用es6
+// filter和indexOf
+// function uniqueArr(arr) {
+//   const res = arr.filter((item, index, array) => {
+//     return array.indexOf(item) === index;
+//   });
+
+//   return res;
+// }
+// const arr = [1, 1, 2, 2, 3, 45, 6, 4, 2, 11];
+// console.log(uniqueArr(arr));
+
+// 空间复杂度O(1)
 function uniqueArr(arr) {
-  var slow = 0;
-  var fast = 0;
+  let slow = 0;
+  let fast = 0;
+  arr.sort((a, b) => {
+    return a - b;
+  });
 
   while (fast < arr.length) {
     fast++;
     if (arr[fast] > arr[slow]) {
       slow++;
-      [arr[slow], arr[fast]] = [arr[fast], arr[slow]];
+      [arr[fast], arr[slow]] = [arr[slow], arr[fast]];
     }
   }
+  arr.length = slow + 1;
 }
+const arr = [1, 1, 2, 2, 3, 45, 6, 4, 2, 11];
+uniqueArr(arr);
+console.log(arr);

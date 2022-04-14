22ae8b30c4af1280ce4753814c6aa148e2088988
zhangzhibang0309 <295854713@qq.com>
2022-04-14 15:51:22 +0800

    冒泡排序

---
+++ JS手写题/13_冒泡排序.js
@@ -0,0 +1,12 @@
+const arr = [3, 6, 2, 4, 1];
+const len = arr.length;
+
+for (let i = 0; i < len - 1; i++) {
+  for (let j = 0; j < len - i - 1; j++) {
+    if (arr[j] > arr[j + 1]) {
+      [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+    }
+  }
+}
+
+console.log(arr);

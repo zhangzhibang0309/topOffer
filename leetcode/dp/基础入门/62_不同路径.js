d8d5785c3c5a36ea10b46594b8b9acc1632b202b
zhangzhibang0309 <295854713@qq.com>
2022-04-15 20:28:38 +0800

    不同路径

--- leetcode/dp/基础入门/62_不同路径.js
+++ leetcode/dp/基础入门/62_不同路径.js
@@ -7,13 +7,10 @@
   // 1. dp[i][j]是到点(i,j)的路线数
   // 2. dp[i][j] = dp[i-1,j] + dp[i,j-1]
   // 3. 遍历顺序从左到右，一层层遍历
-  // 4. 初识化dp[i] = 0
+  // 4. 初识化dp[i] = 1
   // 5. 
   let dp = []
-  for (let i = 0; i < m; i++) dp.push(new Array(n).fill(0))
-
-  for (let i = 0; i < n; i++) dp[0][i] = 1
-  for (let i = 0; i < m; i++) dp[i][0] = 1
+  for (let i = 0; i < m; i++) dp.push(new Array(n).fill(1))
 
   for (let i = 1; i < m; i++)
     for (let j = 1; j < n; j++)

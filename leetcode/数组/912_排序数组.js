170d63af5e0628841cd329fa29d09cf2f9d75e48
zhangzhibang0309 <295854713@qq.com>
2022-04-20 19:01:12 +0800

    排序数组

--- leetcode/数组/912_排序数组.js
+++ leetcode/数组/912_排序数组.js
@@ -5,45 +5,27 @@
  * @return {number[]}
  */
 // 这里只整理了快排
-var sortArray = function (nums) {
-  function quickSort(arr, left = 0, right = arr.length - 1) {
-    if (arr.length > 1) {
-      const lineIndex = partition(arr, left, right);
-      if (left < lineIndex - 1) {
-        quickSort(arr, left, lineIndex - 1);
-      }
-      if (lineIndex < right) {
-        quickSort(arr, lineIndex, right);
-      }
-    }
-    return arr;
-  }
-  function partition(arr, left, right) {
-    let pivotValue = arr[Math.floor(left + (right - left) / 2)];
-    let i = left;
-    let j = right;
-    while (i <= j) {
-      while (arr[i] < pivotValue) {
-        i++;
-      }
-      while (arr[j] > pivotValue) {
-        j--;
-      }
-
-      if (i <= j) {
-        swap(arr, i, j);
-        i++;
-        j--;
-      }
-    }
-    return i;
-  }
-
-  function swap(arr, i, j) {
+// 这种方法理解起来十分有条理，不过力扣上某条数据给卡到了1000+ms
+// 边界情况很清晰
+function quickSort(arr, left = 0, right = arr.length - 1) {
+  if (right <= left) return;
+  let lineIndex = partition(arr, left, right);
+  quickSort(arr, left, lineIndex - 1);
+  quickSort(arr, lineIndex + 1, right);
+  return arr;
+}
+// 每一次分区
+function partition(arr, left, right) {
+  const v = arr[left];
+  let i = left,
+    j = right + 1;
+  while (true) {
+    while (arr[++i] < v) if (i >= right) break;
+    while (arr[--j] > v) if (j <= left) break;
+    if (i >= j) break;
     [arr[i], arr[j]] = [arr[j], arr[i]];
   }
-
-  quickSort(nums);
-
-  return nums;
-};
+  // 四种情况break出来，j都是左子数组的最后一个元素，然后交换
+  [arr[left], arr[j]] = [arr[j], arr[left]];
+  return j;
+}

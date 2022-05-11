04c3170ee5cfda861b29ecdd529e4c6733b90e21
zhangzhibang0309 <295854713@qq.com>
2022-05-11 12:59:10 +0800

    贪心算法

--- 算法/前端啃算法-系统/算法思想/22_贪心算法.js
+++ 算法/前端啃算法-系统/算法思想/22_贪心算法.js
@@ -10,3 +10,21 @@ var canJump = function (nums) {
 
   return false;
 };
+
+// 分发饼干
+var findContentChildren = function (g, s) {
+  g.sort((a, b) => b - a);
+  s.sort((a, b) => b - a);
+
+  // 饼干满足胃口尽可能大的
+  let ret = 0;
+  let index = 0; // 饼干索引
+  for (let i = 0; i < g.length; i++) {
+    if (index < s.length && s[index] >= g[i]) {
+      ret++;
+      index++;
+    }
+  }
+
+  return ret;
+};

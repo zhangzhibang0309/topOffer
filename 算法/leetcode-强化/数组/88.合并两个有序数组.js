5de4d12d90d8783e6502648a26b8fc2689ee2ac1
zhangzhibang0309 <295854713@qq.com>
2022-06-01 15:54:30 +0800

    合并两个有序数组

--- 算法/leetcode-强化/数组/88.合并两个有序数组.js
+++ 算法/leetcode-强化/数组/88.合并两个有序数组.js
@@ -7,18 +7,16 @@
  */
 
 // 双指针，时间复杂度O(n) 空间复杂度O(1)
-var merge = function (nums1, m, nums2, n) {
-  let index1 = m - 1;
-  let index2 = n - 1;
+var merge1 = function (nums1, m, nums2, n) {
+  let top = m - 1;
+  let bottom = n - 1;
+  let index = nums1.length;
 
-  for (let i = 0; i < nums1.length; i++) {
-    // 如果nums1跑完了，那就得单独处理nums2
-    if (index1 < 0) nums1[nums1.length - 1 - i] = nums2[index2--];
-    // 如果nums2先跑完了，那么nums1就不用管了
-    else if (index2 < 0) return;
-    // 都没跑完的话比较，将更大的放入nums1后面
+  while (index >= 0) {
+    if (top < 0) nums1[index--] = nums2[bottom--];
+    else if (bottom < 0) return;
     else
-      nums1[nums1.length - 1 - i] =
-        nums1[index1] > nums2[index2] ? nums1[index1--] : nums2[index2--];
+      nums1[index--] =
+        nums1[top] > nums2[bottom] ? nums1[top--] : nums2[bottom--];
   }
 };
